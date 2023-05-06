12f50011294fe5a6ce66bd4062236b14e41b62b7
Kelvin <kelvinl.ctd@gmail.com>
2023-05-06 21:56:50 +1200

    add acm back, main

---
+++ acm.tf
@@ -0,0 +1,26 @@
+resource "aws_acm_certificate" "cert_acm" {
+  provider          = us-east-1
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_acm_certificate" "cert_acm_data" {
+  domain   = var.domain_name
+  provider = us-east-1
+  depends_on = [
+    aws_acm_certificate_validation.acm_validate
+  ]
+}
+
+resource "aws_acm_certificate_validation" "acm_validate" {
+  provider                = us-east-1
+  certificate_arn         = aws_acm_certificate.cert_acm.arn
+  validation_record_fqdns = [aws_route53_record.r53record.fqdn]
+  depends_on = [
+    aws_route53domains_registered_domain.registereddomain
+  ]
+}
--- main.tf
+++ main.tf
@@ -2,6 +2,21 @@ terraform {
   backend "s3" {
     bucket         = "github-oidc-terraform-aws-tfstate"
     key            = "terraform.tfstate"
-    region         = "ap-southeast-2"
   }
+  # A provider requirement consists of a local name (aws),  source location, and a version constraint. 
+  required_providers {
+    aws = {     
+      # Declaring the source location/address where Terraform can download plugins
+      source  = "hashicorp/aws"
+      # Declaring the version of aws provider as greater than 3.0
+      version = "~> 3.0"  
+    }
+  }
+}
+
+
+# Configuring the AWS Provider in us-east-1 region
+
+provider "aws" {
+  region = "ap-southeast-2"
 }
