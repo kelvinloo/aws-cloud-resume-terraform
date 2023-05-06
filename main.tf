fb8bbcad43e4b1b3b231e55b9b8f36f33b3c39bb
Kelvin <kelvinl.ctd@gmail.com>
2023-05-06 21:58:19 +1200

    changed version

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "github-oidc-terraform-aws-tfstate"
     key            = "terraform.tfstate"
+    region = "ap-southeast-2"
   }
   # A provider requirement consists of a local name (aws),  source location, and a version constraint. 
   required_providers {
@@ -9,7 +10,7 @@ terraform {
       # Declaring the source location/address where Terraform can download plugins
       source  = "hashicorp/aws"
       # Declaring the version of aws provider as greater than 3.0
-      version = "~> 3.0"  
+      version = "~> 4.63.0"  
     }
   }
 }
