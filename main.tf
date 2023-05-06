b4d02be890851b5aff9b6692b2bd1a66f808cf5a
Kelvin <kelvinl.ctd@gmail.com>
2023-05-06 21:52:33 +1200

    main

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.0"
-   }
- }
-}
-
-provider "aws" {
- region = "ap-southeast-2"
-}
-
-terraform {
   backend "s3" {
     bucket         = "github-oidc-terraform-aws-tfstate"
     key            = "terraform.tfstate"
