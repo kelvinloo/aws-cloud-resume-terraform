1e14bcb9e532577b18d8e8bd82727473464ba235
Kelvin <kelvinl.ctd@gmail.com>
2023-05-06 21:43:13 +1200

    added provider

--- providers.tf
+++ providers.tf
@@ -8,19 +8,18 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "~> 4.63.0"
-      region = "ap-southeast-2"
     }
     
   }
 
 }
 // Default aws region 
-//provider "aws" {
- // region     = var.aws_region
- // access_key = var.aws_access_key
- // secret_key = var.aws_secret_key
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   //profile    = "resumeprofile"
-//}
+}
 
 // Additional provider configuration for US East 1 Certificate must be issued here for Cloudfront SSL
 //provider "aws" {
