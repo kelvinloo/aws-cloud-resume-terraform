720bf3c90943fcb3f7725a915431afd17fce1ada
Kelvin <kelvinl.ctd@gmail.com>
2023-05-07 22:40:33 +1200

    policy edit

--- s3.tf
+++ s3.tf
@@ -16,10 +16,10 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
 // Set S3 bucket to public to allow 
 resource "aws_s3_bucket_public_access_block" "hosting_bucket_acl" {
   bucket                  = aws_s3_bucket.hosting_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 /*
